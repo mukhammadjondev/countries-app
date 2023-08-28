e1020166ae2fda9a34f447be07942007855c3dba
mukhammadjondev <muhsdev@gmail.com>
2023-08-28 19:35:57 +0500

    country-detail component optimization

--- src/components/country-detail.jsx
+++ src/components/country-detail.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useCallback, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 import CountryService from "../service/country"
@@ -11,7 +11,7 @@ const CountryDetail = () => {
   const countName = useParams()
   const { countryDetail, isLoading } = useSelector(state => state.country)
 
-  const getCountryDetail = async () => {
+  const getCountryDetail = useCallback(async () => {
     dispatch(getCountryStart())
     try {
       const response = await CountryService.getCountryDetail(countName.name.toLowerCase())
@@ -19,62 +19,70 @@ const CountryDetail = () => {
     } catch (error) {
       dispatch(getCountryFailure(error))
     }
-  }
+  }, [dispatch])
 
   useEffect(() => {
     getCountryDetail()
-  }, [])
+  }, [getCountryDetail])
 
-  console.log(countryDetail)
+  if (!countryDetail?.[0]) {
+    return null
+  }
 
-  // const {name, flags, population, region, subregion, currencies, languages} = countryDetail[0]
+  const {name, flags, population, region, capital, subregion, currencies, languages, borders} = countryDetail[0]
+  const currencyName = Object.values(currencies).map(currency => currency.name)
+  const currencySymbol = Object.values(currencies).map(currency => currency.symbol)
 
   return isLoading ? (
     <Loader />
-  ) : ( countryDetail !== null && <>
-    <div className="container main-container">
+  ):(<div className="container main-container">
       <a className="btn btn-back" onClick={() => navigate('/')}>
           <i className="fa-solid fa-arrow-left"></i>Back
       </a>
       <div className="wrapper">
-        <img className="country-flags__img" src={countryDetail[0]?.flags?.png} width="560" height="401" alt="country flags" />
+        <img className="country-flags__img" src={flags?.png} width="560" height="400" alt="country flags" />
         <div className="text-info__wrapper">
-            <h2 className="country-name__title">{countryDetail[0]?.name?.common}</h2>
-            <div className="card-infolist__wrapper">
-              <ul className="card-infolist">
-                <li className="infolist-item">
-                  <span><b>Native Name: </b>{countryDetail[0]?.name?.common}</span>
-                </li>
-                <li className="infolist-item">
-                  <span><b>Population: </b>{countryDetail[0]?.population}</span>
-                </li>
-                <li className="infolist-item">
-                  <span><b>Region: </b>{countryDetail[0]?.region}</span>
-                </li>
-                <li className="infolist-item">
-                  <span><b>Sub Region: </b>{countryDetail[0]?.subregion}</span>
-                </li>
-                <li className="infolist-item">
-                  <span><b>Capital: </b>{countryDetail[0]?.capital[0]}</span>
-                </li>
-              </ul>
-              <ul className="card-infolist">
-                <li className="infolist-item">
-                  <span><b>Top Level Domain: </b>{}</span>
-                </li>
-                <li className="infolist-item">
-                  <span><b>Currencies: </b>{}</span>
-                </li>
-                <li className="infolist-item">
-                  <span><b>Languages: </b></span>
-                </li>
-              </ul>
-            </div>
-          <div className="border-wrapper"></div>
+          <h2>{name?.common}</h2>
+          <div className="card-infolist__wrapper">
+            <ul className="card-infolist">
+              <li>
+                <span><b>Native Name: </b>{name?.common}</span>
+              </li>
+              <li>
+                <span><b>Population: </b>{population}</span>
+              </li>
+              <li>
+                <span><b>Region: </b>{region}</span>
+              </li>
+              <li>
+                <span><b>Sub Region: </b>{subregion}</span>
+              </li>
+              <li>
+                <span><b>Capital: </b>{capital?.[0]}</span>
+              </li>
+            </ul>
+            <ul className="card-infolist">
+              <li>
+                <span><b>Top Level Domain: </b>{currencySymbol}</span>
+              </li>
+              <li>
+                <span><b>Currencies: </b>{currencyName}</span>
+              </li>
+              <li>
+                <span><b>Languages: </b>{Object.values(languages).join(', ')}</span>
+              </li>
+            </ul>
+          </div>
+          {borders.length > 0 && <div>
+            <span className="border-title">Border Countries:</span>
+            {borders.map(border => (
+              <span key={border} className="btn country-btn">{border}</span>
+            ))}
+          </div>}
         </div>
       </div>
     </div>
-  </>
-)}
+  )
+}
 
 export default CountryDetail
